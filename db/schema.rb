ee43d0961bf49a1307bd898b8dc2d0da8f5a2b31
Geoffrey Gross <geoffreyrgross@gmail.com>
2014-09-18 18:51:29 -0500

    Update User Model, Controller, and Views for new database structure.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140913185438) do
+ActiveRecord::Schema.define(version: 20140918234345) do
 
   create_table "roles", force: true do |t|
     t.string   "name"
@@ -20,8 +20,6 @@ ActiveRecord::Schema.define(version: 20140913185438) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "first_name"
-    t.string   "last_name"
     t.integer  "role_id"
     t.datetime "created_at"
     t.datetime "updated_at"
