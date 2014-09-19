90ecaee38e261fc5009b2a5257f9af57aafff9e5
Geoffrey Gross <geoffreyrgross@gmail.com>
2014-09-18 19:15:30 -0500

    Fixing create method in User controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,14 +4,18 @@ class Users::RegistrationsController < Devise::RegistrationsController
 		super do |resource|
 			@user_role = Role.find_by(name: "user")
 			p = sign_up_params
-			resource.update role_id: @user_role.id
+			if p[:role].nil?
+				resource.update role_id: @user_role.id
+			else
+				resource.update role_id: Role.find_by(name: p[:role].downcase).id
+			end
 		end
 	end
 
 	private
 
   	def sign_up_params
-  		params.require(:user).permit(:email, :password, :password_confirmation)
+  		params.require(:user).permit(:email, :role, :password, :password_confirmation)
   	end
 
   	def account_update_params
