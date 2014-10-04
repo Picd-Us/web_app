c76d69bad24a31f724073cdf6d8bb7add362d47e
Geoffrey Gross <geoffreyrgross@gmail.com>
2014-10-04 14:59:33 -0500

    Removed role id from users table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141004193513) do
+ActiveRecord::Schema.define(version: 20141004195649) do
 
   create_table "users", force: true do |t|
-    t.integer  "role_id"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "email",                  default: "", null: false
