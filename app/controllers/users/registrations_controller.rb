dd05d59a07665154e9c65b18c87fa39c2588c5c8
Geoffrey Gross <geoffreyrgross@gmail.com>
2014-10-13 12:50:16 -0500

    Fix the registration views for users.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,18 +3,23 @@ class Users::RegistrationsController < Devise::RegistrationsController
 	def create
 		super do |resource|
 			p = sign_up_params
-			if p[:role].nil?
-<<<<<<< HEAD
-				resource.update role_id: @user_role.id
-			else
-				resource.update role_id: Role.find_by(name: p[:role].downcase).id
-=======
-				@user_role = Role.find_by(name: "person")
-				resource.update role_id: @user_role.id
-			else
-				@user_role = Role.find_by(name: p[:role] )
-				resource.update role_id: @user_role.id
->>>>>>> sign-up-persons-through-users
+			unless p[:first_name].nil?
+				resource.update first_name: p[:first_name].downcase
+			end
+			unless p[:last_name].nil?
+				resource.update last_name: p[:last_name].downcase
+			end
+		end
+	end
+
+	def update
+		super do |resource|
+			p = account_update_params
+			unless p[:first_name].nil?
+				resource.update first_name: p[:first_name].downcase
+			end
+			unless p[:last_name].nil?
+				resource.update last_name: p[:last_name].downcase
 			end
 		end
 	end
@@ -22,14 +27,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
 	private
 
   	def sign_up_params
-<<<<<<< HEAD
-  		params.require(:user).permit(:email, :role, :password, :password_confirmation)
-=======
-  		params.require(:user).permit(:email, :password, :password_confirmation, :role)
->>>>>>> sign-up-persons-through-users
+  		params.require(:user).permit(:email, :first_name, :last_name, :password, :password_confirmation)
   	end
 
   	def account_update_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:email, :first_name, :last_name, :password, :password_confirmation, :current_password)
   	end
 end
